8119c90b926634b1602ebc6a9b6776b606f255ce
Bobby Groves <bobbygroves1@gmail.com>
2020-08-13 17:58:22 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const searchClient = algoliasearch('6PZ4DAKX3S', '4039afceb67603529e515096f03b0dd1');
+
+
+const search = instantsearch({
+  indexName: 'movies_Production_copy1',
+  searchClient,
+  insightsClient: window.aa
+});
+
+search.addWidgets([
+    instantsearch.widgets.configure({
+      "clickAnalytics": true
+    
+    }),
+  
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#hits-container',
+    templates: {
+      item(hit) {
+        return `
+          
+            <button type="button" class="btn" ${
+              instantsearch.insights('clickedObjectIDsAfterSearch', {
+                eventName: 'Clicked On Hit',
+                objectIDs: [hit.objectID]
+              })
+            }>
+            ${hit.title}
+            </button>
+      
+        `;
+      },
+    },
+    
+    // {
+    //   item: '<button type="button" class="btn">{{{ _highlightResult.title.value }}}</button>',
+    // }
+
+    
+  }),
+  instantsearch.widgets.refinementList({
+      container: '#refinement-list-genre',
+      attribute: 'genre',
+      limit: 7,
+      showMore: true,
+      searchable: true,
+      cssClasses: {
+        list: 'text-primary',}
+    }),
+
+    instantsearch.widgets.refinementList({
+      container: '#refinement-list-rating',
+      attribute: 'rating',
+      sortBy: ["name:desc"],
+      templates: {
+        searchableNoResults: 'No results'
+      },
+      cssClasses: {
+        list: 'text-primary'}
+
+    }),
+    instantsearch.widgets.pagination({
+      container: '#pagination',
+    })
+]);
+
+search.start();
